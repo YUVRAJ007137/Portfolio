8aa4786aa944caf667f48fc7a40aa36489b3c285
Yuvraj Chaudhari <97021369+YUVRAJ007137@users.noreply.github.com>
2025-03-10 11:52:23 +0530

    link fix

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  EkiZR™
+                <a href="https://techdesk.vercel.app/" className="hover:underline">
+                  Tech Desk™
                 </a>
                 . All Rights Reserved.
               </span>
@@ -52,13 +52,13 @@ const ProjectPageLayout = () => (
     <footer>
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
-          </a>
-          . All Rights Reserved.
-        </span>
+       <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © 2025{" "}
+                <a href="https://techdesk.vercel.app/" className="hover:underline">
+                  Tech Desk™
+                </a>
+                . All Rights Reserved.
+              </span>
       </center>
     </footer>
   </>
